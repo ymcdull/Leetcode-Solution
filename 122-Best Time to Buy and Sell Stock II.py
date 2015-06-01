b6c88954c4df312502e3529760e24a9d192356ff
ymcdull <ymcdull@gmail.com>
2015-06-02 00:27:11 +0100

    Create 122-Best Time to Buy and Sell Stock II.py

---
+++ 122-Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,13 @@
+class Solution:
+    # @param {integer[]} prices
+    # @return {integer}
+    def maxProfit(self, prices):
+        n = len(prices)
+        if n <= 1:
+            return 0
+        profit = 0
+        for i in range(1,n):
+            if prices[i] > prices[i-1]:
+                profit += prices[i] - prices[i-1]
+                
+        return profit
