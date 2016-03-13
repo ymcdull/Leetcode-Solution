3efff6e60e18e59b9fae09ad723f5a575ef058d3
Yunfeng Zhu (Frank) <yunfeng.frank.zhu@gmail.com>
2016-03-13 20:35:23 +0000

    Create 326-Power of Three.py

---
+++ 326-Power of Three.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+
+        # if n <= 0:
+        #     return False
+        # while n > 1:
+        #     if n % 3 != 0:
+        #         return False
+        #     n = n / 3
+        # return True
+        
+        if n <= 0:
+            return False
+        else:
+            return 1162261467 % n == 0
